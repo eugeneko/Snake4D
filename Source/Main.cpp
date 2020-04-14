fc93f2532b3b4e9e25f93f55549e58f4e3fd83dc
Eugene Kozlov <nekoeuge@gmail.com>
2020-04-14 19:26:25 +0300

    Extract code from Start function. Add basic UI.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,6 +6,226 @@
 
 using namespace Urho3D;
 
+using StartCallback = std::function<void()>;
+using PausedCallback = std::function<void(bool paused)>;
+
+class GameUI : public Object
+{
+    URHO3D_OBJECT(GameUI, Object);
+
+public:
+    GameUI(Context* context) : Object(context) {}
+
+    void Initialize(bool startGame, StartCallback startCallback, PausedCallback pauseCallback)
+    {
+        startCallback_ = startCallback;
+        pauseCallback_ = pauseCallback;
+
+        CreateUI();
+        if (startGame)
+            StartGame();
+    }
+
+    void TogglePaused()
+    {
+        if (state_ == State::Paused)
+        {
+            state_ = State::Running;
+            pauseCallback_(false);
+            window_->SetVisible(false);
+        }
+        else if (state_ == State::Running)
+        {
+            state_ = State::Paused;
+            pauseCallback_(true);
+            window_->SetVisible(true);
+        }
+    }
+
+    void StartGame()
+    {
+        state_ = State::Running;
+        startCallback_();
+        pauseCallback_(false);
+        window_->SetVisible(false);
+    }
+
+    static void RegisterObject(Context* context)
+    {
+        context->RegisterFactory<GameUI>();
+    }
+
+private:
+    enum class State { Menu, Paused, Running };
+    void CreateUI()
+    {
+        auto ui = context_->GetSubsystem<UI>();
+        UIElement* uiRoot = ui->GetRoot();
+
+        // Load style
+        auto cache = GetSubsystem<ResourceCache>();
+        auto style = cache->GetResource<XMLFile>("UI/DefaultStyle.xml");
+        uiRoot->SetDefaultStyle(style);
+
+        // Create the Window and add it to the UI's root node
+        window_ = uiRoot->CreateChild<Window>("Window");
+
+        // Set Window size and layout settings
+        window_->SetLayout(LM_VERTICAL, padding_, { padding_, padding_, padding_, padding_ });
+        window_->SetAlignment(HA_CENTER, VA_CENTER);
+        window_->SetStyleAuto();
+
+        // Create buttons
+        Button* resumeButton = CreateButton("Resume", window_);
+        Button* newGameButton = CreateButton("New Game", window_);
+        Button* tutorialButton = CreateButton("Tutorial", window_);
+        Button* demoButton = CreateButton("Demo", window_);
+        Button* exitButton = CreateButton("Exit", window_);
+
+        // Create events
+        SubscribeToEvent(E_KEYDOWN,
+            [this](StringHash eventType, VariantMap& eventData)
+        {
+            if (eventData[KeyDown::P_KEY].GetInt() == KEY_ESCAPE)
+            {
+                TogglePaused();
+            }
+        });
+
+        SubscribeToEvent(resumeButton, E_PRESSED,
+            [this](StringHash eventType, VariantMap& eventData)
+        {
+            TogglePaused();
+        });
+
+        SubscribeToEvent(exitButton, E_PRESSED,
+            [this](StringHash eventType, VariantMap& eventData)
+        {
+            SendEvent(E_EXITREQUESTED);
+        });
+    }
+
+    Button* CreateButton(const ea::string& text, UIElement* parent) const
+    {
+        auto button = parent->CreateChild<Button>(text);
+        button->SetLayout(LM_HORIZONTAL, padding_, { padding_, padding_, padding_, padding_ });
+        button->SetHorizontalAlignment(HA_CENTER);
+        button->SetStyleAuto();
+
+        auto buttonText = button->CreateChild<Text>(text + " Text");
+        buttonText->SetAlignment(HA_CENTER, VA_CENTER);
+        buttonText->SetText(text);
+        buttonText->SetStyleAuto();
+        buttonText->SetFontSize(menuFontSize_);
+
+        button->SetFixedWidth(CeilToInt(buttonText->GetRowWidth(0) + 2 * padding_));
+        button->SetFixedHeight(buttonText->GetHeight() + padding_);
+
+        return button;
+    }
+
+    const int padding_{ 18 };
+    const float menuFontSize_{ 24 };
+
+    StartCallback startCallback_;
+    PausedCallback pauseCallback_;
+
+    State state_{};
+    Window* window_{};
+};
+
+using RenderCallback = std::function<bool(float timeStep, Scene4D& scene4D)>;
+
+class GameRenderer : public Object
+{
+    URHO3D_OBJECT(GameRenderer, Object);
+
+public:
+    GameRenderer(Context* context) : Object(context) {}
+
+    void Initialize(RenderCallback renderCallback)
+    {
+        auto cache = context_->GetCache();
+        auto input = context_->GetInput();
+        auto renderer = context_->GetRenderer();
+
+        scene_ = MakeShared<Scene>(context_);
+        scene_->CreateComponent<Octree>();
+        scene_->CreateComponent<DebugRenderer>();
+
+        auto solidMaterial = MakeShared<Material>(context_);
+        solidMaterial->SetCullMode(CULL_NONE);
+        solidMaterial->SetNumTechniques(1);
+        solidMaterial->SetTechnique(0, cache->GetResource<Technique>("Techniques/NoTextureUnlit.xml"));
+        solidMaterial->SetShaderParameter("MatDiffColor", Color::WHITE);
+        solidMaterial->SetVertexShaderDefines("VERTEXCOLOR");
+        solidMaterial->SetPixelShaderDefines("VERTEXCOLOR");
+
+        auto transparentMaterial = MakeShared<Material>(context_);
+        transparentMaterial->SetCullMode(CULL_NONE);
+        transparentMaterial->SetNumTechniques(1);
+        transparentMaterial->SetTechnique(0, cache->GetResource<Technique>("Techniques/NoTextureUnlitAlpha.xml"));
+        transparentMaterial->SetShaderParameter("MatDiffColor", Color::WHITE);
+        transparentMaterial->SetVertexShaderDefines("VERTEXCOLOR");
+        transparentMaterial->SetPixelShaderDefines("VERTEXCOLOR");
+
+        Node* customGeometryNode = scene_->CreateChild("Custom Geometry");
+        auto solidGeometry = customGeometryNode->CreateComponent<CustomGeometry>();
+        solidGeometry->SetMaterial(solidMaterial);
+        auto transparentGeometry = customGeometryNode->CreateComponent<CustomGeometry>();
+        transparentGeometry->SetMaterial(transparentMaterial);
+
+        // Create zone
+        if (auto zone = scene_->CreateComponent<Zone>())
+        {
+            zone->SetAmbientColor(Color::WHITE);
+            zone->SetBoundingBox(BoundingBox(-1000, 1000));
+            zone->SetFogColor(Color(0x00BFFF, Color::ARGB));
+        }
+
+        // Create actor
+        if (Node* actorNode = scene_->CreateChild("Actor"))
+        {
+            Node* cameraNode = actorNode->CreateChild("Camera");
+            actorNode->SetPosition(Vector3(0.0f, 3.0f, -6.0f));
+            cameraNode->LookAt(Vector3(0, 0, 0));
+
+            auto camera = cameraNode->CreateComponent<Camera>();
+            camera_ = camera;
+        }
+
+        input->SetMouseVisible(true);
+
+        // Update loop
+        SubscribeToEvent(E_UPDATE, [=](StringHash eventType, VariantMap& eventData)
+        {
+            const float timeStep = eventData[Update::P_TIMESTEP].GetFloat();
+            renderCallback(timeStep, scene4D_);
+
+            solidGeometry->BeginGeometry(0, TRIANGLE_LIST);
+            transparentGeometry->BeginGeometry(0, TRIANGLE_LIST);
+
+            BuildScene4D(CustomGeometryBuilder{ solidGeometry, transparentGeometry }, scene4D_);
+
+            solidGeometry->Commit();
+            transparentGeometry->Commit();
+        });
+
+        viewport_ = MakeShared<Viewport>(context_);
+        viewport_->SetScene(scene_);
+        viewport_->SetCamera(camera_);
+
+        if (renderer)
+            renderer->SetViewport(0, viewport_);
+    }
+
+private:
+    SharedPtr<Viewport> viewport_;
+    SharedPtr<Scene> scene_;
+    Scene4D scene4D_;
+    WeakPtr<Camera> camera_;
+};
+
 enum class RelativeRotation4D
 {
     None,
@@ -89,6 +309,7 @@ private:
 class GameWorld
 {
 public:
+    GameWorld() = default;
     explicit GameWorld(int size) : size_(size) { Reset(); }
 
     void Reset()
@@ -106,7 +327,7 @@ public:
     {
         static const ColorTriplet snakeColor{ Color::WHITE, Color::RED, Color::BLUE };
         static const ColorTriplet targetColor{ Color::GREEN, { 1.0f, 1.0f, 0.0f }, { 0.0f, 1.0f, 1.0f } };
-        static const ColorTriplet borderColor{ Color::WHITE, Color::WHITE, Color::WHITE };
+        static const ColorTriplet borderColor{ { 1.0f, 1.0f, 1.0f, 0.3f }, Color::WHITE, Color::WHITE };
 
         // Update camera
         const float cameraTranslationFactor = Clamp(blendFactor * renderSettings_.cameraTranslationSpeed_, 0.0f, 1.0f);
@@ -173,7 +394,7 @@ public:
                             + direction * halfSize
                             + xAxis * (x - halfSize + 0.5f)
                             + yAxis * (y - halfSize + 0.5f);
-                        scene.solidQuads_.push_back(Quad{ position, xAxis * 0.1f, yAxis * 0.1f, borderColor });
+                        scene.solidQuads_.push_back(Quad{ position, xAxis * 0.8f, yAxis * 0.8f, borderColor });
                     }
                 }
             }
@@ -247,18 +468,16 @@ public:
     void Start() override;
 
 private:
-    SharedPtr<Viewport> viewport_;
-    SharedPtr<Scene> scene_;
+    SharedPtr<GameUI> gameUI_;
+    SharedPtr<GameRenderer> gameRenderer_;
 
-    Scene4D scene4D_;
-    GameWorld world_{ 11 };
+    GameWorld gameWorld_;
 
     bool paused_{};
 
     float updatePeriod_{ 1.0f };
     float timeAccumulator_{};
 
-    WeakPtr<Camera> camera_;
 };
 
 void MainApplication::Setup()
@@ -273,88 +492,23 @@ void MainApplication::Start()
     auto renderer = GetSubsystem<Renderer>();
     auto cache = GetSubsystem<ResourceCache>();
 
-    input->SetMouseVisible(true);
-
-    SubscribeToEvent(E_KEYDOWN,
-        [this](StringHash eventType, VariantMap& eventData)
+    auto renderCallback = [=](float timeStep, Scene4D& scene4D)
     {
-        if (eventData[KeyDown::P_KEY].GetInt() == KEY_ESCAPE)
-        {
-            scene_.Reset();
-            SendEvent(E_EXITREQUESTED);
-        }
-    });
-
-    scene_ = MakeShared<Scene>(context_);
-    scene_->CreateComponent<Octree>();
-    scene_->CreateComponent<DebugRenderer>();
-
-    auto solidMaterial = MakeShared<Material>(context_);
-    solidMaterial->SetCullMode(CULL_NONE);
-    solidMaterial->SetNumTechniques(1);
-    solidMaterial->SetTechnique(0, cache->GetResource<Technique>("Techniques/NoTextureUnlit.xml"));
-    solidMaterial->SetShaderParameter("MatDiffColor", Color::WHITE);
-    solidMaterial->SetVertexShaderDefines("VERTEXCOLOR");
-    solidMaterial->SetPixelShaderDefines("VERTEXCOLOR");
-
-    auto transparentMaterial = MakeShared<Material>(context_);
-    transparentMaterial->SetCullMode(CULL_NONE);
-    transparentMaterial->SetNumTechniques(1);
-    transparentMaterial->SetTechnique(0, cache->GetResource<Technique>("Techniques/NoTextureUnlitAlpha.xml"));
-    transparentMaterial->SetShaderParameter("MatDiffColor", Color::WHITE);
-    transparentMaterial->SetVertexShaderDefines("VERTEXCOLOR");
-    transparentMaterial->SetPixelShaderDefines("VERTEXCOLOR");
-
-    Node* customGeometryNode = scene_->CreateChild("Custom Geometry");
-    auto solidGeometry = customGeometryNode->CreateComponent<CustomGeometry>();
-    solidGeometry->SetMaterial(solidMaterial);
-    auto transparentGeometry = customGeometryNode->CreateComponent<CustomGeometry>();
-    transparentGeometry->SetMaterial(transparentMaterial);
-
-    // Create zone
-    if (auto zone = scene_->CreateComponent<Zone>())
-    {
-        zone->SetAmbientColor(Color::WHITE);
-        zone->SetBoundingBox(BoundingBox(-1000, 1000));
-        zone->SetFogColor(Color(0x00BFFF, Color::ARGB));
-    }
-
-    // Create actor
-    if (Node* actorNode = scene_->CreateChild("Actor"))
-    {
-        Node* cameraNode = actorNode->CreateChild("Camera");
-        actorNode->SetPosition(Vector3(0.0f, 3.0f, -6.0f));
-        cameraNode->LookAt(Vector3(0, 0, 0));
-
-        auto camera = cameraNode->CreateComponent<Camera>();
-        camera_ = camera;
-    }
-
-    // Update loop
-    SubscribeToEvent(E_UPDATE, [=](StringHash eventType, VariantMap& eventData)
-    {
-        if (!scene_)
-            return;
-
         // Apply input
         auto input = context_->GetSubsystem<Input>();
-        const float timeStep = eventData[Update::P_TIMESTEP].GetFloat();
-
-        if (input->GetKeyPress(KEY_1))
-            paused_ = !paused_;
 
         if (input->GetKeyPress(KEY_A))
-            world_.SetNextRotation(RelativeRotation4D::Left);
+            gameWorld_.SetNextRotation(RelativeRotation4D::Left);
         if (input->GetKeyPress(KEY_D))
-            world_.SetNextRotation(RelativeRotation4D::Right);
+            gameWorld_.SetNextRotation(RelativeRotation4D::Right);
         if (input->GetKeyPress(KEY_W))
-            world_.SetNextRotation(RelativeRotation4D::Up);
+            gameWorld_.SetNextRotation(RelativeRotation4D::Up);
         if (input->GetKeyPress(KEY_S))
-            world_.SetNextRotation(RelativeRotation4D::Down);
+            gameWorld_.SetNextRotation(RelativeRotation4D::Down);
         if (input->GetKeyPress(KEY_Q))
-            world_.SetNextRotation(RelativeRotation4D::Red);
+            gameWorld_.SetNextRotation(RelativeRotation4D::Red);
         if (input->GetKeyPress(KEY_E))
-            world_.SetNextRotation(RelativeRotation4D::Blue);
+            gameWorld_.SetNextRotation(RelativeRotation4D::Blue);
 
         // Tick scene
         if (!paused_)
@@ -363,28 +517,29 @@ void MainApplication::Start()
         while (timeAccumulator_ >= updatePeriod_)
         {
             timeAccumulator_ -= updatePeriod_;
-            world_.Tick();
+            gameWorld_.Tick();
         }
 
         // Update scene
-        world_.Render(scene4D_, timeAccumulator_ / updatePeriod_);
+        gameWorld_.Render(scene4D, timeAccumulator_ / updatePeriod_);
+        return true;
+    };
 
-        solidGeometry->BeginGeometry(0, TRIANGLE_LIST);
-        transparentGeometry->BeginGeometry(0, TRIANGLE_LIST);
+    auto startCallback = [=]()
+    {
+        gameWorld_ = GameWorld{ 11 };
+    };
 
-        BuildScene4D(CustomGeometryBuilder{ solidGeometry, transparentGeometry }, scene4D_);
+    auto pauseCallback = [=](bool paused)
+    {
+        paused_ = paused;
+    };
 
-        solidGeometry->Commit();
-        transparentGeometry->Commit();
-    });
+    gameUI_ = MakeShared<GameUI>(context_);
+    gameUI_->Initialize(true, startCallback, pauseCallback);
 
-    viewport_ = MakeShared<Viewport>(context_);
-    viewport_->SetScene(scene_);
-    viewport_->SetCamera(camera_);
-
-    if (renderer)
-        renderer->SetViewport(0, viewport_);
-
+    gameRenderer_ = MakeShared<GameRenderer>(context_);
+    gameRenderer_->Initialize(renderCallback);
 }
 
 URHO3D_DEFINE_APPLICATION_MAIN(MainApplication);
